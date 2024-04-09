5bb230e666fafca6af5cdf55cf65c62685999364
Carlos Arias <carlos.ariasalmanzar@gmail.com>
2024-04-08 20:47:36 -0400

    mejorando comentarios

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import os
-from dotenv import load_dotenv
-import telebot
-from file_reader import read_message_from_file
+import os  # Importa el módulo os para interactuar con el sistema operativo.
+from dotenv import load_dotenv  # Importa la función load_dotenv del paquete dotenv para cargar variables de entorno.
+import telebot  # Importa la biblioteca telebot para interactuar con la API de Telegram.
+from file_reader import read_message_from_file  # Importa la función read_message_from_file del módulo file_reader para leer mensajes desde archivos.
 
+# Carga las variables de entorno desde el archivo .env
 load_dotenv()
 
 # Obtiene el token del bot desde las variables de entorno
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-# Lista de comandos disponibles
+# Lista de comandos disponibles para el bot
 available_commands = [
     "/help", "/start", "/quienes_somos", "/mision", "/vision",
     "/valores", "/historia", "/rector", "/calidad_en_la_gestion",
@@ -19,8 +20,13 @@ available_commands = [
 botsito = telebot.TeleBot(TOKEN)
 
 # Manejador de mensajes para el comando /help y /start
+# Responde con un mensaje inicial que describe el bot y los comandos disponibles
 @botsito.message_handler(commands=["help", "start"])
 def mensaje_inicial(message):
+    """
+    Manejador de mensajes para los comandos /help y /start.
+    Responde con un mensaje inicial que describe el bot y los comandos disponibles.
+    """
     # Mensaje inicial que se envía al usuario al iniciar o solicitar ayuda
     initial_message = "¡Hola! ¿Cómo estás?\n \n"
     initial_message += "Este bot es para consultar información sobre el Instituto Tecnológico de las Américas (ITLA) \n \n"
@@ -39,64 +45,101 @@ def mensaje_inicial(message):
     botsito.reply_to(message, initial_message)
 
 # Manejador de mensajes para el comando /quienes_somos
+# Lee el mensaje desde un archivo y lo envía como respuesta
 @botsito.message_handler(commands=["quienes_somos"])
 def mensaje_quienes_somos(message):
-    # Lee el mensaje desde un archivo y lo envía como respuesta
+    """
+    Manejador de mensajes para el comando /quienes_somos.
+    Lee el mensaje desde un archivo y lo envía como respuesta.
+    """
     mensaje = read_message_from_file("quienes_somos.txt")
     botsito.reply_to(message, mensaje)
 
 # Manejador de mensajes para el comando /historia
+# Lee el mensaje desde un archivo y lo envía como respuesta
 @botsito.message_handler(commands=["historia"])
 def mensaje_historia(message):
+    """
+    Manejador de mensajes para el comando /historia.
+    Lee el mensaje desde un archivo y lo envía como respuesta.
+    """
     mensaje = read_message_from_file("historia.txt")
-
     botsito.reply_to(message, mensaje)
 
 # Manejador de mensajes para el comando /rector
+# Lee el mensaje desde un archivo y lo envía como respuesta
 @botsito.message_handler(commands=["rector"])
 def mensaje_rector(message):
+    """
+    Manejador de mensajes para el comando /rector.
+    Lee el mensaje desde un archivo y lo envía como respuesta.
+    """
     mensaje = read_message_from_file("rector.txt")
-
     botsito.reply_to(message, mensaje)
 
 # Manejador de mensajes para el comando /calidad_en_la_gestion
+# Lee el mensaje desde un archivo y lo envía como respuesta
 @botsito.message_handler(commands=["calidad_en_la_gestion"])
 def mensaje_calidad(message):
+    """
+    Manejador de mensajes para el comando /calidad_en_la_gestion.
+    Lee el mensaje desde un archivo y lo envía como respuesta.
+    """
     mensaje = read_message_from_file("calidad_gestion.txt")
-
     botsito.reply_to(message, mensaje)
 
 # Manejador de mensajes para el comando /mision
+# Lee el mensaje desde un archivo y lo envía como respuesta
 @botsito.message_handler(commands=["mision"])
 def mensaje_mision(message):
+    """
+    Manejador de mensajes para el comando /mision.
+    Lee el mensaje desde un archivo y lo envía como respuesta.
+    """
     mensaje = read_message_from_file("mision.txt")
-
     botsito.reply_to(message, mensaje)
 
 # Manejador de mensajes para el comando /vision
+# Lee el mensaje desde un archivo y lo envía como respuesta
 @botsito.message_handler(commands=["vision"])
 def mensaje_vision(message):
+    """
+    Manejador de mensajes para el comando /vision.
+    Lee el mensaje desde un archivo y lo envía como respuesta.
+    """
     mensaje = read_message_from_file("vision.txt")
-
     botsito.reply_to(message, mensaje)
 
 # Manejador de mensajes para el comando /valores
+# Lee el mensaje desde un archivo y lo envía como respuesta
 @botsito.message_handler(commands=["valores"])
 def mensaje_valores(message):
+    """
+    Manejador de mensajes para el comando /valores.
+    Lee el mensaje desde un archivo y lo envía como respuesta.
+    """
     mensaje = read_message_from_file("valores.txt")
-
     botsito.reply_to(message, mensaje)
 
 # Manejador de mensajes para el comando /carreras_tecnologicas
+# Lee el mensaje desde un archivo y lo envía como respuesta
 @botsito.message_handler(commands=["carreras_tecnologicas"])
 def mensaje_valores(message):
+    """
+    Manejador de mensajes para el comando /carreras_tecnologicas.
+    Lee el mensaje desde un archivo y lo envía como respuesta.
+    """
     mensaje = read_message_from_file("carreras.txt")
-
     botsito.reply_to(message, mensaje)
 
 # Manejador de mensajes para el comando /identidad_corporativa
+# Envía colores corporativos y logos corporativos como respuesta
 @botsito.message_handler(commands=["identidad_corporativa"])
 def identidad_corporativa(message):
+    """
+    Manejador de mensajes para el comando /identidad_corporativa.
+    Envía colores corporativos y logos corporativos como respuesta.
+    """
     # Envía un mensaje con los colores corporativos
     colores = "Colores Corporativos: \n"
     colores += """
@@ -120,8 +163,13 @@ def identidad_corporativa(message):
     botsito.send_media_group(message.chat.id, media)
 
 # Manejador de mensajes para completar comandos
+# Sugerir comandos similares si el comando ingresado no es válido
 @botsito.message_handler(func=lambda message: True)
 def autocomplete_commands(message):
+    """
+    Manejador de mensajes para completar comandos.
+    Sugerir comandos similares si el comando ingresado no es válido.
+    """
     # Convierte el texto del mensaje en minúsculas
     text = message.text.lower()
 
